ac37f7503302fd76ef624f55ed3fe3f1d58b89dd
07012779666 <56950993+07012779666@users.noreply.github.com>
2020-01-16 15:43:30 +0900

    新規作成2.2
    2.2

--- GIGI/src/gut/Gut1.java
+++ GIGI/src/gut/Gut1.java
@@ -4,6 +4,11 @@ public class Gut1 {
 
 	public static void main(String[] args) {
 		System.out.println("123456");
+		int a = 20;
+		int b = 30;
+		if(a < b){
+			System.out.println("nihao");
+		}
 
 	}
 
