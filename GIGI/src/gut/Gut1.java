abd7333a48d3a486fc887043e48d2475eacabfd1
txq6163 <txq1559240028@yahoo.co.jp>
2020-01-16 15:37:32 +0900

    更新
    1.1

--- GIGI/src/gut/Gut1.java
+++ GIGI/src/gut/Gut1.java
@@ -5,6 +5,12 @@ public class Gut1 {
 	public static void main(String[] args) {
 		System.out.println("123456");
 
+		int a, b, c;
+		a = 1;
+		b = 2;
+		c = 3;
+		System.out.println(a+b+c);
+
 	}
 
 }
